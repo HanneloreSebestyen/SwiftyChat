4c4a07de90fb0d85000b03a127f107fc24804930
Hanne Sebestyen <hanne.sebestyen@mobiversal.com>
2022-12-08 13:11:27 +0200

    stop loadingMore

--- Sources/SwiftyChat/ChatView.swift
+++ Sources/SwiftyChat/ChatView.swift
@@ -100,6 +100,7 @@ public struct ChatView<Message: ChatMessage, User: ChatUser>: View {
                                 print(index)
                                 if index == 9 && isScrolledUp {
                                    loadMore = true
+                                    isScrolledUp = false
                                 }
                             }
                     }
